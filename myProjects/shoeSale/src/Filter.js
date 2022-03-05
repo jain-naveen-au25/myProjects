ead6bdad7d3a30da789ced2f0f6025705ca1f4ec
jain-naveen-au25 <87427392+jain-naveen-au25@users.noreply.github.com>
2022-03-05 11:57:03 +0530

    Create Filter.js

---
+++ myProjects/shoeSale/src/Filter.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Filters=()=>{
+  return(
+    <React.Fragment>
+      <div>
+        <h4>Category</h4>
+      </div>
+    </React.Fragment>
+  )
+}
+export default Filters;
